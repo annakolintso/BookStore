cfd8daf127294f659daaf5c62085d35150a77a14
annakolintso <anna.kolintso@gmail.com>
2025-08-06 16:21:54 +0300

    added @gorhom/bottom-sheet

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { StatusBar, StyleSheet, View, Text, Button } from 'react-native';
 import { useFonts } from 'expo-font';
 import { SplashScreen } from 'expo-router';
 import { Provider } from 'react-redux';
@@ -8,6 +8,12 @@ import { store } from './store';
 import { AuthProvider } from "./src/contexts/AuthContext";
 // import { TranslatorProvider } from 'react-translate';
 // import translations from './translations';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 
 const scopeOfFonts = {
   'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
@@ -20,6 +26,14 @@ const scopeOfFonts = {
 
 const App = () => {
   const [loaded, error] = useFonts(scopeOfFonts);
+  const bottomSheetModalRef = useRef(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
 
   React.useEffect(() => {
     if (loaded || error) {
@@ -32,15 +46,50 @@ const App = () => {
   }
 
   return (
-    <Provider store={store}>
-      {/* <TranslatorProvider translations={translations}> */}
+    <GestureHandlerRootView style={styles.container}>
+      <Provider store={store}>
+        {/* <TranslatorProvider translations={translations}> */}
+
         <AuthProvider>
-          <StatusBar style="auto" />
-          <Navigation />
+          <BottomSheetModalProvider>
+            <StatusBar style="auto" />
+            <Navigation />
+
+
+            <Button
+              onPress={handlePresentModalPress}
+              title="Modal"
+              color="white"
+            />
+            <BottomSheetModal
+              ref={bottomSheetModalRef}
+              onChange={handleSheetChanges}
+            >
+              <BottomSheetView style={styles.contentContainer}>
+                <Text>🎉</Text>
+              </BottomSheetView>
+            </BottomSheetModal>
+          </BottomSheetModalProvider>
         </AuthProvider>
-      {/* </TranslatorProvider> */}
-    </Provider>
+
+        {/* </TranslatorProvider> */}
+      </Provider>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 24,
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 24,
+  },
+});
 
 export default App;
